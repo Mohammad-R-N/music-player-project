049578df428dc41c712408f827836dfcc9ad3d40
Mr-Coder <mohammad.ca@proton.me>
2023-09-03 16:29:05 -0400

    feat:otp page

--- muisc-project/accounts/views.py
+++ muisc-project/accounts/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'accounts/home/home.html')
+
+def otp(request):
+    return render(request,'accounts/login/otp.html')
