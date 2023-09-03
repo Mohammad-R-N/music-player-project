1a20c99c571fefc7ec1661152f4bd3bf9a5cf2ef
Mr-Coder <mohammad.ca@proton.me>
2023-09-03 08:27:25 -0400

    Configs

--- muisc-project/musics/models.py
+++ muisc-project/musics/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 
-class Genre(models.Model):
-    pass
+# class Genre(models.Model):
+#     name=models.CharField(max_length=50)
 
-class Artist(models.Model):
-    pass
-
-
-class Song(models.Model):
-    pass
+# class Artist(models.Model):
+#     name=models.CharField(max_length=100)
+#     bio=models.TextField()
+#     image=models.ImageField()
+    
+# class Song(models.Model):
+#     title=
+#     artist=
+#     upload_date=
