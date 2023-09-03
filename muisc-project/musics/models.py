53a5c16818098a218d651a97d7499bc84992455e
Mr-Coder <mohammad.ca@proton.me>
2023-09-03 09:33:27 -0400

    chore:models have deleted temporary

--- muisc-project/musics/models.py
+++ muisc-project/musics/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 
 
-# class Genre(models.Model):
-#     name=models.CharField(max_length=50)
+class Genre(models.Model):
+    pass
 
-# class Artist(models.Model):
-#     name=models.CharField(max_length=100)
-#     bio=models.TextField()
-#     image=models.ImageField()
-    
-# class Song(models.Model):
-#     title=
-#     artist=
-#     upload_date=
+class Artist(models.Model):
+    pass
+
+class Song(models.Model):
+    pass
