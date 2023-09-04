970f3afbf2f3f09880929a80cb50fbb5909ec197
Mr-Coder <mohammad.ca@proton.me>
2023-09-04 11:37:03 -0400

    feat: all pages have completed

--- muisc-project/musics/urls.py
+++ muisc-project/musics/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns=[
     path('playlist-detail/',playlist_detail,name='playlist-detail'),
+    path('song-detail/',song_detail,name="song-detail")
     ]
--- muisc-project/musics/views.py
+++ muisc-project/musics/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def playlist_detail(request):
     return render(request,'musics/allplaylists/playlist-detail.html')
+
+def song_detail(request):
+    return render(request,'musics/songdetail/songdetail.html')
